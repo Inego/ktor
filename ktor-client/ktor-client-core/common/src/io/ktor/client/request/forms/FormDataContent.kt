e182963b56966c76bb9f5268ac068c66afbd9c4c
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-06-16 15:02:47 +0300

    Fix empty multipart post
        Close #1901

--- ktor-client/ktor-client-core/common/src/io/ktor/client/request/forms/FormDataContent.kt
+++ ktor-client/ktor-client-core/common/src/io/ktor/client/request/forms/FormDataContent.kt
@@ -86,26 +86,18 @@ class MultiPartFormDataContent(
     override val contentType: ContentType = ContentType.MultiPart.FormData.withParameter("boundary", boundary)
 
     init {
-        var rawLength = rawParts.fold<PreparedPart, Long?>(0L) { current, part ->
-            val size = part.size
-            if (current != null && size != null) {
-                current + size
-            } else {
-                null
-            }
+        var rawLength = rawParts.fold<PreparedPart, Long>(0L) { current, part ->
+            val size = part.size ?: 0
+            current + size
         }
 
-        if (rawLength != null && rawLength != 0L) {
-            rawLength += BODY_OVERHEAD_SIZE
-        }
+        rawLength += BODY_OVERHEAD_SIZE
 
         contentLength = rawLength
     }
 
     override suspend fun writeTo(channel: ByteWriteChannel) {
         try {
-            if (rawParts.isEmpty()) return
-
             channel.writeFully(RN_BYTES)
             channel.writeFully(RN_BYTES)
 
