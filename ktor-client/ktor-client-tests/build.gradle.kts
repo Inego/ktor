96f5a863337d1ac122237815b4c5ee880226512a
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2020-06-16 15:57:00 +0300

    fixup! Move the default test server to CIO

--- ktor-client/ktor-client-tests/build.gradle.kts
+++ ktor-client/ktor-client-tests/build.gradle.kts
@@ -54,6 +54,8 @@ kotlin.sourceSets {
     jvmMain {
         dependencies {
             api(project(":ktor-server:ktor-server-cio"))
+            api(project(":ktor-server:ktor-server-netty"))
+            api(project(":ktor-server:ktor-server-jetty"))
             api(project(":ktor-features:ktor-auth"))
             api(project(":ktor-features:ktor-websockets"))
             api("ch.qos.logback:logback-classic:$logback_version")
